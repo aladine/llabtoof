97e43845e6263c98fdf433452a9723b1c5be7294
adrienberaud <adrienberaud@32fa2b17-f343-9600-210b-b6c6d10dda99>
2010-10-21 08:22:44 +0000

    Update IO, add io server and player parts

---
+++ io/io_server.h
@@ -0,0 +1,28 @@
+/* io_server.h -- io convertion functions for server
+ * 
+ * Copyright 2010 Per Kristian Kjoell <a0075664@nus.edu.sg>
+ * Copyright 2010 Adrien Béraud <adrienberaud@gmail.com>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+
+typedef struct io_server_manager IOServermanager;
+
+void IOServer_init(IOServermanager * io, IOmanager_cb callback);
+
+void IOServer_send(IOServermanager * io, GameState * output);
+
+
+
