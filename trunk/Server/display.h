c39993c4ee262db2ca35ab86db5a04c07da02b1d
dan.aladine@gmail.com <dan.aladine@gmail.com@32fa2b17-f343-9600-210b-b6c6d10dda99>
2010-10-28 13:58:29 +0000

    Interfrgate timer and display tft

--- trunk/Server/display.h
+++ trunk/Server/display.h
@@ -1,6 +1,3 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
 #include "xparameters.h"
 #include "xio.h"
 #include "xtft.h"
@@ -10,11 +7,13 @@
 #define GREEN 0x0000ff00
 #define RED 0x00ff0000
 #define BLUE 0x000000ff
+#define WHITE 0x00ffffff
+#define BLACK 0x00000000  
+#define YELLOW 0x00ffff00
+#define TEAM_A 0
+#define TEAM_B 1
+XTft TftInstance;
 
-static XTft TftInstance;
-
-int X(int );
-int Y(int );
 int XTft_DrawSolidBox(XTft *, int , int , int , int , unsigned int);
 int Draw_Player(XTft *, int , int ,unsigned int );
 int Draw_Border(XTft *);
@@ -24,5 +23,3 @@ int in_circle(int x, int y);
 int Draw_Circle(XTft *Tft,  unsigned int col);
 void Init(XTft *TftInstance);
 int TftDrawLine(XTft *, int , int ,int, int , unsigned int);
-
-#endif
