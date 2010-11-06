683a5a075aa89aa3adbceee488ff75704aa84033
adrienberaud <adrienberaud@32fa2b17-f343-9600-210b-b6c6d10dda99>
2010-11-06 03:35:51 +0000

    Syntax fix for io.c

--- trunk/io/io.c
+++ trunk/io/io.c
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *              http://www.apache.org/licenses/LICENSE-2.0
+ *	      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -36,53 +36,51 @@ void IO_receivedHandler(IOmanager * io, unsigned received);
 
 void IO_init(IOmanager * io, u16 device_id, u16 interrupt_id, IO_cb callback, void* callback_arg)
 {
-        int status;
+	int status;
 
-        io->init = FALSE;
-        io->callback = callback;
-        io->callback_arg = callback_arg;
+	io->init = FALSE;
+	io->callback = callback;
+	io->callback_arg = callback_arg;
 
-        status = XUartLite_Initialize(&(io->uartlite), device_id);
-        if(status != XST_SUCCESS) 
-        {
-                xil_printf(" Error : Xuartlite_initialize failed. Code : %d\n",status);
-                return;
-        }
-        
-        status = XUartLite_SelfTest(&(io->uartlite));
-        if(status != XST_SUCCESS)
-        {
-                xil_printf(" Error : Xuartlite_selftest failed. Code : %d\n",status);
-                return;
-        }
-        
-        XUartLite_ResetFifos(&(io->uartlite));
-        
-//status = XIntc_Connect(interrupt_controller, interrupt_id, (XInterruptHandler)XUartLite_InterruptHandler, &(io->uartlite));
-		  status = register_int_handler(XPAR_XPS_INTC_0_RS232_DCE_INTERRUPT_INTR, (void*)XUartLite_InterruptHandler, &(io->uartlite));
-		  if (status != XST_SUCCESS)
-        {
-                xil_printf(" Error : register_int_handler failed. Code : %d\n",status);
-                return;
-        }
+	status = XUartLite_Initialize(&(io->uartlite), device_id);
+	if(status != XST_SUCCESS) 
+	{
+		xil_printf(" Error : Xuartlite_initialize failed. Code : %d\n",status);
+		return;
+	}
+	
+	status = XUartLite_SelfTest(&(io->uartlite));
+	if(status != XST_SUCCESS)
+	{
+		xil_printf(" Error : Xuartlite_selftest failed. Code : %d\n",status);
+		return;
+	}
+	
+	XUartLite_ResetFifos(&(io->uartlite));
+	
+	status = register_int_handler(XPAR_XPS_INTC_0_RS232_DCE_INTERRUPT_INTR, (void*)XUartLite_InterruptHandler, &(io->uartlite));
+	if (status != XST_SUCCESS)
+	{
+		xil_printf(" Error : register_int_handler failed. Code : %d\n",status);
+		return;
+	}
 		  
-		   enable_interrupt(interrupt_id);
+	enable_interrupt(interrupt_id);
 			
-			XUartLite_EnableInterrupt(&(io->uartlite));
-        
-        // Launch input thread (that will pool for new data)
-        status = pthread_create(&io->input_t, NULL, (void*)IO_inputThread, io);
-        
-        io->init = TRUE;
-      //  xil_printf("\n  IO_init in io.c finished.   \n");
-
+	XUartLite_EnableInterrupt(&(io->uartlite));
+	
+	// Launch input thread (that will pool for new data)
+	status = pthread_create(&io->input_t, NULL, (void*)IO_inputThread, io);
+	
+	io->init = TRUE;
+    //  xil_printf("\n  IO_init in io.c finished.   \n");
 }
 
 void IO_send(IOmanager * io, void * data)
 {
-        if(!io || !(io->init) || !data) return;
+	if(!io || !(io->init) || !data) return;
 
-        XUartLite_Send(&(io->uartlite), data, 4);
+	XUartLite_Send(&(io->uartlite), data, 4);
 }
 
 
@@ -95,44 +93,40 @@ void IO_send(IOmanager * io, void * data)
  */
 void IO_inputThread(IOmanager * io)
 {
-        if(!io) return;
+	if(!io) return;
 
-        char i;
-                  
-        char * ib_start = &(io->input_buffer_start);
-        char * ib_size = &(io->input_buffer_received);
+	char i;
+		  
+	char * ib_start = &(io->input_buffer_start);
+	char * ib_size = &(io->input_buffer_received);
 
-        for(i=0; i<BUFFER_SIZE; i++) io->input_buffer[i] = 0;   //empty input buffer
-        
-        *ib_start = 0;
-        *ib_size = 0;
-        
-        XUartLite_SetRecvHandler(&(io->uartlite), (XUartLite_Handler)IO_receivedHandler, io);
-        IO_receivedHandler(io, 0);
-        
-        //main pooling thread
-      //  xil_printf(" IO_inputThread started.");
-        while(1)
-        {
-                // 2 next lines now done with interupts
-                //received = XUartLite_Recv(&(io->uartlite), io->input_buffer+((ib_start+ib_size)%BUFFER_SIZE), BUFFER_SIZE-((ib_start+ib_size)%BUFFER_SIZE));
-                //ib_size += received;
-
-                while(*ib_size >= PACKET_SIZE)
-                {
-                        io->callback(io->callback_arg, io->input_buffer+(*ib_start%BUFFER_SIZE));
-                        ib_start += PACKET_SIZE;
-                        ib_size -= PACKET_SIZE;         //possibility of multi-"threading" problems here
-                }
-        
-        }
+	for(i=0; i<BUFFER_SIZE; i++) io->input_buffer[i] = 0;   //empty input buffer
+	
+	*ib_start = 0;
+	*ib_size = 0;
+	
+	XUartLite_SetRecvHandler(&(io->uartlite), (XUartLite_Handler)IO_receivedHandler, io);
+	IO_receivedHandler(io, 0);
+	
+	//main pooling thread
+    //  xil_printf(" IO_inputThread started.");
+	while(1)
+	{
+		while(*ib_size >= PACKET_SIZE)
+		{
+			io->callback(io->callback_arg, io->input_buffer+(*ib_start%BUFFER_SIZE));
+			ib_start += PACKET_SIZE;
+			ib_size -= PACKET_SIZE;	 //possibility of multi-"threading" problems here
+		}
+	
+	}
 }
 
 void IO_receivedHandler(IOmanager * io, unsigned received)
 {		  
-        io->input_buffer_received += received;
+	io->input_buffer_received += received;
 		  
-        XUartLite_Recv(&(io->uartlite), 
-                       io->input_buffer+((io->input_buffer_start+io->input_buffer_received)%BUFFER_SIZE), 
-                       PACKET_SIZE);
+	XUartLite_Recv(&(io->uartlite), 
+		       io->input_buffer+((io->input_buffer_start+io->input_buffer_received)%BUFFER_SIZE), 
+		       PACKET_SIZE);
 }
