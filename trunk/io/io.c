ac53602eb2ab15f453bdbc6dbfe5aef26738b9db
adrienberaud <adrienberaud@32fa2b17-f343-9600-210b-b6c6d10dda99>
2010-11-06 06:12:45 +0000

    Syntax fix for io.c

--- trunk/io/io.c
+++ trunk/io/io.c
@@ -41,18 +41,18 @@ void IO_init(IOmanager * io, u16 device_id, u16 interrupt_id, IO_cb callback, vo
 	io->init = FALSE;
 	io->callback = callback;
 	io->callback_arg = callback_arg;
-
+	
 	status = XUartLite_Initialize(&(io->uartlite), device_id);
 	if(status != XST_SUCCESS) 
 	{
-		xil_printf(" Error : Xuartlite_initialize failed. Code : %d\n",status);
+		xil_printf(" Error : Xuartlite_initialize failed. Code : %d\n", status);
 		return;
 	}
 	
 	status = XUartLite_SelfTest(&(io->uartlite));
 	if(status != XST_SUCCESS)
 	{
-		xil_printf(" Error : Xuartlite_selftest failed. Code : %d\n",status);
+		xil_printf(" Error : Xuartlite_selftest failed. Code : %d\n", status);
 		return;
 	}
 	
@@ -61,12 +61,12 @@ void IO_init(IOmanager * io, u16 device_id, u16 interrupt_id, IO_cb callback, vo
 	status = register_int_handler(XPAR_XPS_INTC_0_RS232_DCE_INTERRUPT_INTR, (void*)XUartLite_InterruptHandler, &(io->uartlite));
 	if (status != XST_SUCCESS)
 	{
-		xil_printf(" Error : register_int_handler failed. Code : %d\n",status);
+		xil_printf(" Error : register_int_handler failed. Code : %d\n", status);
 		return;
 	}
-		  
+	
 	enable_interrupt(interrupt_id);
-			
+	
 	XUartLite_EnableInterrupt(&(io->uartlite));
 	
 	// Launch input thread (that will pool for new data)
@@ -114,18 +114,19 @@ void IO_inputThread(IOmanager * io)
 	{
 		while(*ib_size >= PACKET_SIZE)
 		{
-			io->callback(io->callback_arg, io->input_buffer+(*ib_start%BUFFER_SIZE));
+			io->callback(io->callback_arg, io->input_buffer+(*ib_start % BUFFER_SIZE));
 			ib_start += PACKET_SIZE;
 			ib_size -= PACKET_SIZE;	 //possibility of multi-"threading" problems here
 		}
-	
+		
+		//for(i=0; i<1000; i++) { asm("nop"); asm("nop"); }
 	}
 }
 
 void IO_receivedHandler(IOmanager * io, unsigned received)
 {		  
 	io->input_buffer_received += received;
-		  
+	
 	XUartLite_Recv(&(io->uartlite), 
 		       io->input_buffer+((io->input_buffer_start+io->input_buffer_received)%BUFFER_SIZE), 
 		       PACKET_SIZE);
